6c0a9bd1f4f8798741dc7e3ee03a9f5bb9da9dca
Victor Varza <vvarza@adobe.com>
2018-11-23 11:01:27 +0200

    update repo

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run -d -p 4001:4001 --name etcd trainersontheweb/etcd:3.3.4
+docker run -d -p 4001:4001 --name etcd qedzone/etcd:3.3.4
 docker run -d -p 31380:80 --link etcd:etcd --name phippy local/phippy
