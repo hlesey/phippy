c32c1655154f61359ae583a740bc7bdecf443bc8
Victor Varza <victor.varza@gmail.com>
2022-03-12 21:49:44 +0200

    fix default ingressclass

--- load-test/run.sh
+++ load-test/run.sh
@@ -1,23 +1,35 @@
 #!/usr/bin/env bash
 
-URL="http://phippy.clusterx.qedzone.ro:30080"
+UI_URL="http://phippy.clusterx.qedzone.ro:30080"
+API_URL="http://phippy-api.clusterx.qedzone.ro:30080"
+
 
 # Generating POST requests
 echo "Generating POST requests"
 echo "------------------------"
-
-hey -n 1000 -m POST $URL
-
+hey -n 1000 -m POST $UI_URL
 
 # Generating GET requests
 echo "Generating GET requests"
 echo "------------------------"
+hey -n 1000 -m POST $UI_URL
 
-hey -n 1000 -m POST $URL
+# Generating GET requests
+echo "Generating GET requests for version endpoint"
+echo "------------------------"
+hey -n 1000 -m POST $UI_URL/version
 
+# Generating GET requests
+echo "Generating GET requests just for api readyz endpoint"
+echo "------------------------"
+hey -n 1000 -m POST $API_URL/readyz
+
+# Generating GET requests
+echo "Generating GET requests just for api livez endpoint"
+echo "------------------------"
+hey -n 1000 -m POST $API_URL/livez
 
 # Artificially generate errors in the app
 echo "Artificially generate errors"
 echo "------------------------"
-
-hey -n 1000 -m GET "$URL/trigger_error"
+hey -n 1000 -m GET "$API_URL/trigger_error"
