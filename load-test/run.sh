b20d7dd227f4b7661bd0925d0b0fdae62e93ad2d
Victor Varza <victor.varza@gmail.com>
2022-03-13 18:46:04 +0200

    Improve monitoring (#7)
    * add better monitoring
    
    * fix default ingressclass
    
    * add k8s probes

--- load-test/run.sh
+++ load-test/run.sh
@@ -1,33 +1,17 @@
 #!/usr/bin/env bash
 
-URL="http://phippy-api.clusterx.qedzone.ro:30080"
+UI_URL="http://phippy.clusterx.qedzone.ro:30080"
+API_URL="http://phippy-api.clusterx.qedzone.ro:30080"
 
-# Generating POST requests
-echo "Generating POST requests"
+
+echo "Generating requests for POST /"
 echo "------------------------"
-sleep 1;
+hey -n 1000 -m POST $UI_URL
 
-for i in {0..500}; do
-    echo "running: curl -s -XPOST $URL"
-    curl -s -XPOST $URL
-done
-
-# Generating GET requests
-echo "Generating GET requests"
+echo "Generating requests for GET /"
 echo "------------------------"
-sleep 1;
+hey -n 1000 -m GET $UI_URL
 
-for i in {0..1000}; do
-    echo "running: curl -s -XGET $URL"
-    curl -s -XGET $URL
-done 
-
-# Artificially generate errors in the app
-echo "Artificially generate errors"
+echo "Generating requests for api GET /trigger_error"
 echo "------------------------"
-sleep 1;
-
-for i in {0..20}; do
-    echo "running: curl -s -XGET "$URL/trigger_error""
-    curl -s -XGET "$URL/trigger_error"
-done
+hey -n 1000 -m POST "$API_URL/trigger_error"
