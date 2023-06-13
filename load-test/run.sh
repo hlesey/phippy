f741fc6a5bbae04ba0866a5307925e9827c77fd0
Victor Varza <victor.varza@gmail.com>
2023-06-13 18:49:19 +0300

    update load test script

--- load-test/run.sh
+++ load-test/run.sh
@@ -3,15 +3,21 @@
 UI_URL="http://phippy.clusterx.qedzone.ro:30080"
 API_URL="http://phippy-api.clusterx.qedzone.ro:30080"
 
+ITERATIONS=10000
 
-echo "Generating requests for POST /"
-echo "------------------------"
-hey -n 1000 -m POST $UI_URL
+for i in $(seq 1 $ITERATIONS); do
 
-echo "Generating requests for GET /"
-echo "------------------------"
-hey -n 1000 -m GET $UI_URL
+    echo "Generating requests for POST /"
+    echo "------------------------"
+    hey -n 1000 -m POST $UI_URL
 
-echo "Generating requests for api GET /trigger_error"
-echo "------------------------"
-hey -n 1000 -m POST "$API_URL/trigger_error"
+    echo "Generating requests for GET /"
+    echo "------------------------"
+    hey -n 1000 -m GET $UI_URL
+
+    echo "Generating requests for api GET /trigger_error"
+    echo "------------------------"
+    hey -n 10 -c 2 -m POST "$API_URL/trigger_error"
+
+    sleep $(jot -r 1 40 130)
+done
